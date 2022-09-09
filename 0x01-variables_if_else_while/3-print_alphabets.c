44f9a43a7de5492dedc544e12113f53db689dd52
John <johnpops59@gmail.com>
2022-09-09 11:10:56 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Printing alphabets in two cases
+ *
+ * Return 0(success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j < 91; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
