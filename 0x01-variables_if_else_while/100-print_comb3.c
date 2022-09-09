70dd82d8fe7c390a1ae4f1b7ff6ca0fd6e12fd6c
John <johnpops59@gmail.com>
2022-09-09 13:35:34 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all combinations of two digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 48;
+	b = 48;
+
+	while (e < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			if (b != a && b < i)
+			{
+				putchar(b);
+				putchar(a);
+				if (a == 57 && b == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			a++;
+		}
+		b++;
+	}
+	putchar('\n')
+		return (0);
+}
