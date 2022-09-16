fa3960f42fcc94f7370f2d2ef251f3471169d0b9
John <johnpops59@gmail.com>
2022-09-15 18:43:16 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 0 - 14 10 times
+ */
+void more_numbers(void)
+{
+	int i, j, count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			j = i;
+
+			if (j > 9)
+			{
+				putchar(19);
+				j = j % 10;
+			}
+			putchar(j + 48);
+		}
+		putchar('\n');
+	}
+}
