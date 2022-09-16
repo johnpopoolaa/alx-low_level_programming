243542928edbf3f97fc47c3b1e6331b8208bceb0
John <johnpops59@gmail.com>
2022-09-15 18:14:09 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers from 0 - 9 excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; 1++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
