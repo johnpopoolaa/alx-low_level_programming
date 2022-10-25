29aee3a9e5443be0d8829f41244693fbd494280d
John <johnpops59@gmail.com>
2022-10-25 13:55:52 -0700

    commit 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,11 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- * *free_listint2 - frees a linked list
- * *@head: pointer to the head of the list
- * *
- * *Return: void
- * */
+ *free_listint2 - frees a linked list
+ *@head: pointer to the head of the list
+ *
+ *Return: void
+ */
 
 void free_listint2(listint_t **head)
 {
