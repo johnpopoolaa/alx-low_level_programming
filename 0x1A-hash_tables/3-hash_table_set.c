a7756ef09fc6de01acb03fcc7d956e12d7b60b44
John Popoola <johnpops59@gmail.com>
2023-01-07 20:54:24 +0100

    commit message

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,16 +1,24 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
+/**
+ * hash_table: adds element to hash table
+ * @ht: hash table to be updated
+ * @key: the key, string
+ * @value: the value corresponding to the key
+ * Return: 1 if successful, else 0
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 	char *value_copy;
+	unsigned long int index, i;
 
 	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 	value_copy = strdup(value);
 	if (!value_copy)
 		return (0);
-	index = key_index((const unsugned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
@@ -21,21 +29,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
 	{
 		free(value_copy);
 		return (0);
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 	{
-		free(new);
+		free(new_node);
 		return (0);
 	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new_node->value = value_copy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
